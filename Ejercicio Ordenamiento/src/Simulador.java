436cd733f5370d1d67b8ae07661e993285faa829
Marcos Cifuentes <marcosnaza16@gmail.com>
2017-04-05 10:51:05 -0300

    commit 2 Ejercicio ordenamiento

--- Ejercicio Ordenamiento/src/Simulador.java
+++ Ejercicio Ordenamiento/src/Simulador.java
@@ -1,52 +1,66 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 
 public class Simulador {
 
 	public static void main(String[] args) {
-		
-		final int MAX = 5000;
-		
+
+		//Constante que brinda el maximo del arreglo		
+		final int MAX =100000;
+		//Constante del minimo del arreglo
 		final int MIN = 0;
-		
+		//Elementos tipo DateFormat usados para calcular el tiempo de ejecucion
+		final DateFormat sdf1 = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss:S");
+
+		final DateFormat sdf2 = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss:S");
+		//Arreglos del ejercicio
 		ListaConArreglo Lista1 = new ListaConArreglo(MAX);
-		
+
 		ListaConArreglo Lista2 = new ListaConArreglo(MAX);
-		
+		//Llamado a funcion para cargar los arreglos
 		Lista1.cargarArreglo();
-		
+
 		Lista2.cargarArreglo();
-		
-		System.out.println("Lista 1 desordenada");
-		
-		System.out.println(Lista1.toString());
-		
-		System.out.println("Lista 2 desordenada");
+		//Inicio de ordenamientos y calculos de tiempos de ejecucion
+		Date v_start1 = new Date();
 
-		System.out.println(Lista2.toString());
-		
-		long startTime1 = System.nanoTime();
+		System.out.println(sdf1.format(v_start1));
+		//Funcion que ordena por Mergesort a un arreglo
 		Lista1.ordenamientoMergesort(MIN, MAX-1);
-		long endTime1 = System.nanoTime();
 
-		long duration1 = (endTime1 - startTime1);
-		
-		System.out.println(duration1+" milisegundos");
-		
-		System.out.println("Lista 1 ordenada");
-		
-		System.out.println(Lista1.toString());
-		
-		long startTime2 = System.nanoTime();
-		Lista2.ordanamientoBurbujeo();
-		long endTime2 = System.nanoTime();
+		Date v_end1 = new Date();
 
-		long duration2 = (endTime2 - startTime2);
+		System.out.println(sdf1.format(v_end1));
+
+		//Muestra del tiempo de ejecucuion
+		System.out.println("Lista 1 ordenada en ");
+
+		System.out.println((v_end1.getTime() - v_start1.getTime())+ " milisegundos");
+
+		Date v_start2 = new Date();
+
+		System.out.println(sdf2.format(v_start2));
+		//Funcion que ordena por Burbujeo a un arreglo
 		Lista2.ordanamientoBurbujeo();
-		
-		System.out.println(duration2+" milisegundos");
-		
-		System.out.println("Lista 2 ordenada");
-		
-		System.out.println(Lista2.toString());
+
+		Date v_end2 = new Date();
+
+		System.out.println(sdf2.format(v_end2));
+
+		//Muestra del tiempo de ejecucuion
+		System.out.println("Lista 2 ordenada en ");
+
+		System.out.println((v_end2.getTime() - v_start2.getTime())+ " milisegundos");
+
+		//Luego de hacer unas pruebas con distintas cantidades de valores, pude ver que cuando los arreglos poseen un tamaño 
+		//de 100000 valores, el ordenamiento por Mergesort comienza a dar tiempos constantes de 16 milisegundos 
+		//(desde los 10000 daba resultados de entre 0 hasta los 16 milisegundos, en el peor de los casos), con pruebas mayores,  
+		//los tiempos aumentan pero a un ritmo reducido con una complejidad de O(n log n), en comparacion del ordenamento por 
+		//burbujeo, que a los 100000 valores, le toma unos 30000 milisegundos en completarse, y con los distintos valores 
+		//muestra una compejidad de O(nXn)
+
 	}
 
 }
