19a87d1a064479fbb1e385f411332adbbb8bb5ba
marcos cifuentes <marcosnaza16@gmail.com>
2017-04-12 18:48:49 -0300

    4 commit entrega

--- EjercicioGrafo/src/grafoDirigido.java
+++ EjercicioGrafo/src/grafoDirigido.java
@@ -16,7 +16,7 @@ public class grafoDirigido {
 		return vertices[i].size();
 	}
 
-	public void addVertice(int indice){
+	public void addVertice(int indice){//agrega vertices
 
 		if(indice >= vertices.length){
 			Lista[] auxiliar = new Lista[indice * 2];
@@ -28,10 +28,9 @@ public class grafoDirigido {
 			}
 		}
 		vertices[indice] = new Lista();
-
 	}
 
-	public void addArista(int i, int j){
+	public void addArista(int i, int j){//agrega arista a un vertice definido
 		if(i < vertices.length && j < vertices.length){
 			vertices[i].addNodo(j);;
 		}
@@ -41,7 +40,7 @@ public class grafoDirigido {
 		return vertices[v].at(i);
 	}
 
-	public boolean tieneCicloIterativo(){
+	public boolean tieneCicloIterativo(){//funcion que recorre el grafo iterativamente y retorna si el grafo es ciclico
 
 		Pila pila = new Pila();
 
@@ -50,14 +49,11 @@ public class grafoDirigido {
 			auxiliar[i] = new Vertice(i,Estado.NoVisitado);
 		}
 
-
 		pila.apilar(0);
 
 		while (!pila.estaVacia()) {
 			if(auxiliar[pila.verTope()].getEstado() == Estado.NoVisitado){
 				auxiliar[pila.verTope()].setEstado(Estado.Visitandose);
-
-
 				if(vertices[pila.verTope()].size()>0){
 					for (int i = 0; i < vertices[pila.verTope()].size(); i++) {
 						if(auxiliar[vertices[pila.verTope()].at(i)].getEstado() == Estado.NoVisitado){
@@ -65,28 +61,22 @@ public class grafoDirigido {
 						}else if(auxiliar[vertices[pila.verTope()].at(i)].getEstado() == Estado.Visitandose){
 							return true;
 						}
-
 					}
 				}else{
 					auxiliar[pila.verTope()].setEstado(Estado.Visitado);
 					pila.desapilar();
 				}
-
-
-
 			}else if(auxiliar[pila.verTope()].getEstado() == Estado.Visitado){
 				pila.desapilar();
 			}else{
 				auxiliar[pila.verTope()].setEstado(Estado.Visitado);
 				pila.desapilar();
 			}
-
 		}
-
 		return false;
 	}
 
-	public boolean tieneCicloRecursivo(){
+	public boolean tieneCicloRecursivo(){//funcion que retorna si el grafo es ciclico, utilizando la funcion recursiva
 		Vertice[] auxiliar = new Vertice [vertices.length];
 		for (int i = 0; i < auxiliar.length; i++) {
 			auxiliar[i] = new Vertice(i,Estado.NoVisitado);
@@ -102,8 +92,8 @@ public class grafoDirigido {
 		return false;
 	}
 
-	private boolean DFS_Visit(Vertice v,int t, Vertice[] auxiliar){
-
+	private boolean DFS_Visit(Vertice v,int t, Vertice[] auxiliar){//funcion recursiva que recorre el grafo
+			
 		v.setEstado(Estado.Visitandose);
 		v.setInicio(t);
 		t=t+1;
@@ -115,22 +105,19 @@ public class grafoDirigido {
 			}else if(auxiliar[pos].getEstado() == Estado.Visitandose){
 				return true;
 			}
-
 		}
 
 		v.setEstado(Estado.Visitado);
 		return false;
 	}
 
-	public void addAristas(){
+	public void addAristas(){//agrega aristas a todo el grafo dinamicamente
 		int i = 0;
 		while( i < vertices.length-1){
 			this.addArista(i, i+2);
 			this.addArista(i, i+1);
-			
+
 			i++;
 		}
 	}
-
-
 }
